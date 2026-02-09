45269160c2b55cf207316ed7a728f6bc34ca3830
shirafune04 <chanshirafune@gmail.com>
2026-02-09 19:35:45 +0300

    Lesson12

---
+++ src/main/java/Ll13.java
@@ -0,0 +1,15 @@
+public class Ll13 {
+    public static void main(String[] args) {
+        int size = 3;
+        int[][] matrix = new int[size][size];
+        for (int i = 0; i < size; i++) {
+            matrix[i][i] = 1;
+        }
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ src/main/java/ll14.java
@@ -0,0 +1,9 @@
+import java.awt.*;
+import java.sql.Time;
+import java.util.Date;
+
+public class ll14 {
+    public static void main(String[] args){
+
+    }
+}
