fc94d11c5451a4b65d4c187fa8eca5fde4612176
shirafune04 <chanshirafune@gmail.com>
2026-02-09 20:36:48 +0300

    Исправила L11.java

--- src/main/java/L1.java
+++ src/main/java/L1.java
@@ -1,4 +1,11 @@
-package PACKAGE_NAME;
-
 public class L1 {
+    public static void printThreeWords() {
+        System.out.println("Orange");
+        System.out.println("Banana");
+        System.out.println("Apple");
+    }
+    public static void main(String[] args) {
+        printThreeWords();
+    }
 }
+
--- src/main/java/L11.java
+++ src/main/java/L11.java
@@ -1,4 +1,11 @@
-package PACKAGE_NAME;
-
 public class L11 {
+    public static void main(String[] args) {
+        int[] array = new int[100];
+        for (int i = 0; i < 100; i++) {
+            array[i] = i + 1;
+            System.out.print(array[i] + " ");
+        }
+    }
 }
+
+
