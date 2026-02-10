90b614819c7d8850e62af888ad03ef3a9833af5e
shirafune04 <chanshirafune@gmail.com>
2026-02-10 17:53:07 +0300

    1-14

--- src/main/java/L14.java
+++ src/main/java/L14.java
@@ -1,5 +1,15 @@
-public class L14 {
-    public static void main(String[] args){
+import java.util.Arrays;
 
+public class L14 {
+    public static int[] createArray (int len, int initialValue) {
+        int [] array = new int[len];
+        Arrays.fill(array, initialValue);
+        return array;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(createArray(1, -4)));
+        System.out.println(Arrays.toString(createArray(7, 5)));
+        System.out.println(Arrays.toString(createArray(5, 15)));
     }
 }
