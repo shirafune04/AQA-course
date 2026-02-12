15deca832c9530feb6f284671be0f03337834d21
shirafune04 <chanshirafune@gmail.com>
2026-02-12 12:09:08 +0300

    2

--- src/main/java/Park.java
+++ src/main/java/Park.java
@@ -1,4 +1,28 @@
-package PACKAGE_NAME;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Park {
+    private List<Attraction> attractions = new ArrayList<>();
+
+    public class Attraction {
+        private String name;
+        private String workTime;
+        private double price;
+
+        public Attraction(String name, String workTime, double price) {
+            this.name = name;
+            this.workTime = workTime;
+            this.price = price;
+        }
+
+        public void printInfo() {
+            System.out.println(name + " | " + workTime + " | " + price + " руб.");
+        }
+    }
+
+    public static void main(String[] args) {
+        Park park = new Park();
+        Park.Attraction a = park.new Attraction("Колесо обозрения", "10:00-22:00", 450);
+        a.printInfo();
+    }
 }
--- src/main/java/Product1.java
+++ src/main/java/Product1.java
@@ -1,4 +1,12 @@
 public class Product1 {
-    Product1 [] productsArray = new Product1[5];
+    public static void main(String[] args) {
 
+        Product[] productsArray = new Product[5];
+
+        productsArray[0] = new Product("Samsung S25 Ultra", "01.02.2025", "Samsung Corp.", "Korea", 5599, true);
+        productsArray[1] = new Product("iPhone 17 Pro", "19.09.2025", "Apple.", "USA", 7000, false);
+        productsArray[2] = new Product("Google Pixel 9", "10.02.2025", "Google", "США", 799, false);
+        productsArray[3] = new Product("Xiaomi 14 Ultra", "20.01.2025", "Xiaomi", "Китай", 899, true);
+        productsArray[4] = new Product("OnePlus 12", "05.02.2025", "OnePlus", "Китай", 699, true);
+    }
 }
